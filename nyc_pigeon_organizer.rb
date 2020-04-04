73640f675451e65cb2001e7b1870699a2fa51f44
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-04 21:48:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = {}
+  
+  data.each do |key, value|
+    value.each do |item, names|
+      names.each do |name|
+        organized_hash[name] ||= {}
+        organized_hash[name][key] ||= []
+        organized_hash[name][key] << item.to_s
+      end 
+    end
+  end
+  organized_hash
 end
