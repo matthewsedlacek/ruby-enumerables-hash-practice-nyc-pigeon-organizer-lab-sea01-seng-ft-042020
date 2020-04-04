3867251f50d66ceb01d737d95f018274f1cb9480
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-04-04 21:47:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,14 @@
 def nyc_pigeon_organizer(data)
-  data.reduce ({}) do |memo, pair|
-p memo
-p pair
-memo
+  organized_hash = {}
+  
+  data.each do |key, value|
+    value.each do |item, names|
+      names.each do |name|
+        organized_hash[name] ||= {}
+        organized_hash[name][key] ||= []
+        organized_hash[name][key] << item.to_s
+      end 
+    end
+  end
+  organized_hash
 end
